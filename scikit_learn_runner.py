33af73924694d5f44ae651951cdfdf95ed1cc4f3
Justin Cunningham <justin@bulletprooftiger.com>
2014-12-01 22:32:04 -0800

    Added option to save regularization stats

--- scikit_learn_runner.py
+++ scikit_learn_runner.py
@@ -5,11 +5,13 @@ Usage:
   scikit_learn_runner.py [options]
 
 Options:
-  -h --help             Show this screen.
-  --feature-file=FILE   Json file containing features [default: data/features.json]
+  -h --help                     Show this screen.
+  --feature-file=FILE           Json file containing features [default: data/features.json]
+  --save-regularization-stats   Save a regularization csv file.
 """
 
 from docopt import docopt
+import csv
 import json
 import os
 import helpers
@@ -30,7 +32,17 @@ class ScikitLearnRunner(object):
         self.run_model(linear_model.LogisticRegression(C=0.8), "logistic_regressor")
         self.run_model(linear_model.SGDRegressor(shuffle=True, n_iter=100000), "sgd_regressor")
 
-        #self.run_model(svm.SVR(), "svm")
+        iters = [10, 100, 1000, 10000, 100000]
+        alpha = [0, 0.0001, 0.001, 0.01, 0.05, 0.1, 0.25, 0.5]
+
+        if self.arguments['--save-regularization-stats']:
+            with open('data/regularization.csv', 'w') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(['Iteration', 'Alpha', 'Train Error', 'Test Error'])
+                for iter in iters:
+                    for a in alpha:
+                        train_error, test_error = self.run_model(linear_model.SGDRegressor(shuffle=True, alpha=a, n_iter=iter, random_state=42), "sgd_regressor_%s_%s" % (iter, a))
+                        writer.writerow([iter, a, train_error, test_error])
 
     def load_data(self):
         self.data = json.load(open(self.arguments['--feature-file']))
@@ -57,12 +69,18 @@ class ScikitLearnRunner(object):
         predictions = model.predict(self.predict_feature_matrix)
         logging.info("Predicting using model %s done! Took %s seconds." % (model_name, time.time() - start))
 
-        helpers.standard_eror(predictions, self.predict_labels)
+        logging.info("Test error:")
+        test_error = helpers.standard_eror(predictions, self.predict_labels)
+
+        logging.info("Train error:")
+        train_error = helpers.standard_eror(model.predict(self.training_feature_matrix), self.training_labels)
 
         # Write result to file
         f = open('data/result/%s' % model_name, 'w')
         f.write(pickle.dumps(model))
         f.close()
+
+        return train_error, test_error
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
